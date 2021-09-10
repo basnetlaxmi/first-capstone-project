d39f2163ba12af0990e9b3fddc3f054889d300d3
basnetlaxmi <ddsanzel@gmail.com>
2021-09-10 13:23:18 +0545

    Add about page

--- feature.js
+++ feature.js
@@ -43,16 +43,27 @@ const speakers = [
     biography: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.'
     + '(Main books: The Wealth of the Network , Penguins and Leviathan )',
   },
+
+  {
+    image: './images/speaker_05.png',
+    alt: 'Speaker-5',
+    name: 'yohai benkler',
+    designation: 'Professor at Harvard Law School',
+    biography: 'Focusing on a collaborative approach in a networked environment, he created the concept of co-production based on sharing, such as open source software and Wikipedia.'
+    + '(Main books: The Wealth of the Network , Penguins and Leviathan )',
+  },
 ];
 
 const speakersContainer = document.querySelector('.speakers');
 
 function speaker(s) {
-  return `<div class="speaker">
+  return `<div class="speaker d-flex justify-content-around mt-3 ps-3">
     <img src="${s.image}" alt="${s.alt}">
-    <h3>${s.name}</h3>
-    <h5>${s.designation}</h5>
-    <p>${s.biography}</p>
+        <div class="ps-2">
+        <h3>${s.name}</h3> 
+        <h5>${s.designation}</h5>
+        <p class="w-75">${s.biography}</p>
+        </div>
     </div>
  `;
 }
